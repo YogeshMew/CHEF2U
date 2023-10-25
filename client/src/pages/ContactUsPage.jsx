c049ca326c60d7ed2101486e4787821169d93d1d
bose <yrmewara@student.sfit.ac.in>
2023-10-25 22:30:47 +0530

    initial commit

---
+++ client/src/pages/ContactUsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+const ContactUsPage = (() => {
+    return (<>
+        <Navbar/>
+        <ContactUs />
+        <Footer/>
+    </>);
+        
+});
+export default ContactUsPage;
